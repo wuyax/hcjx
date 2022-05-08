6a5753ef75d4363fbf80d1897567240cb10f9068
wuyax <pengqingkai@gmail.com>
2022-05-08 17:23:47 +0800

    update

--- assets/product.cf8f10b4.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index.bb80b049.js";import{a as r,o as t,i as c}from"./vendor.2c0e064a.js";const n={};function _(a,s){const o=r("router-view");return t(),c(o)}var i=e(n,[["render",_]]);export{i as default};
